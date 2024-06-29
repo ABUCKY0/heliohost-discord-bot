852ce92d43fdb224d855d17e8c98139ddafaf343
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2024-06-29 14:07:52 -0500

    fix(auth): :bug: Fix config error in authorization.js

--- utility/authorization.js
+++ utility/authorization.js
@@ -1,14 +1,36 @@
-const config = require('../config.js');
+const { config } = require('../config.js');
 
 module.exports = {
     checkAuth: function (interaction) {
-        const invokedUser = interaction.user.id;
+        try {
+            const invokedUser = interaction.user.id;
 
-        // Check if the user has one of the roles defined in config.auth.roles
-        if (interaction.member.roles.cache.some(role => config.auth.role_ids.includes(role.id)) || config.auth.users.includes(invokedUser)) {
-            return true;
+            // Role ids
+            for (const role of config.auth.role_ids) {
+                if (interaction.member.roles.cache.some(r => r.id === role)) {
+                    return true;
+                }
+            }
+
+            // Role names
+            for (const role of config.auth.role_names) {
+                if (interaction.member.roles.cache.some(r => r.name === role)) {
+                    return true;
+                }
+            }
+
+            // User ids
+            for (const user of config.auth.users) {
+                if (invokedUser === user) {
+                    return true;
+                }
+            }
+
+            return false;
         }
-
-        return false;
+        catch (error) {
+            console.error(error);
+            return false;
+        }
     }
 }
